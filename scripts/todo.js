46e4a7a804714def98583ec7f6deec67d63792f9
rdogm13 <drg6525@yahoo.com>
2020-02-12 00:17:33 -0800

    basic scroll to top button

--- scripts/todo.js
+++ scripts/todo.js
@@ -108,6 +108,21 @@ $(document).ready(function () {
         shiftTask($(this), false);
     })
 
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 80) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    })
+
+    $('.scroll-to-top').click(function() {
+        $('html, body').animation({scrollTop: 0}, 800);
+        return false;
+    })
+
+
     initTaskData();
 
 })
